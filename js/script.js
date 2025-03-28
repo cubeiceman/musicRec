28aba96476a807f37090fd2f1e4e37951e3c28e8
sunshinetown <88341723+cubeiceman@users.noreply.github.com>
2025-03-27 23:51:04 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+let loggedIn = false;
+let settingPref = false;
 
+document.getElementById("updatePreferences").style.visibility = "hidden";
+document.getElementById("preferences").style.visibility = "hidden";
+
+function updateUser() {
+    loggedIn = !loggedIn;
+
+    if (loggedIn) {
+        document.getElementById("user").innerHTML = "logged in!";
+        document.getElementById("preferences").style.visibility = "visible";
+
+    }
+    else {
+        document.getElementById("user").innerHTML = "not logged in!";
+        document.getElementById("preferences").style.visibility = "hidden";
+        document.getElementById("updatePreferences").style.visibility = "hidden";
+    }
+}
+
+function preferenceVisibility() {
+    if (settingPref == false) {
+        settingPref = true;
+        document.getElementById("updatePreferences").style.visibility = "visible";
+    }
+
+    else {
+        settingPref = false;
+        document.getElementById("updatePreferences").style.visibility = "hidden";
+    }
+}
